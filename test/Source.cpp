a2fdbb9e075d0246d19d04224108fa8dd4b4582d
kutselabskii <osutantarian@bk.ru>
2017-07-05 16:38:45 +1000

    Add files via upload

--- test/Source.cpp
+++ test/Source.cpp
@@ -2,70 +2,103 @@
 #include <iostream>
 using namespace std;
 
-namespace Foo { namespace Bar {
-}}
-namespace egorka {}
+const int MYCONST;
 
-void somefunction   (    int a, void *b  )   {return;}
-double anotherfunction     () { int a = 0; int b = a; return (double) b;}
+namespace Foo {
+    namespace Bar {
+    }
+}
+namespace egorka {
+}
 
-SomeClass::Constructor()
-    : aaaaaaaa(aaaaaaaa), aaaaaaaa(aaaaaaaa), aaaaaaaa(aaaaaaaaaaaaaaaaaaaaaaaaa) {
-  return 0;
-}						
+void somEEEfunction(int a, void *b) { return; }
 
-int main(){int n;int i = 0;int j = 0;cout << "\n Vvedite rezmernost matrici";cin >> n;int **matr;matr = new int *[n];if(matr[i]=NULL){		cout<<"Nesozdandinamicheskiymassiv";		return 0;}	for (int i = 0; i < n; i++)	{		matr[i] = new int[n];		if (matr[i] = NULL)		{			cout << "Ne sozdan dinamicheskiy massiv";			return 0;		}	}	for (int i = 0; i < n; i++)	{		for (int j = 0; j < n; j++)		{			cout << "vvedite massiv";			cin >> matr[i][j];		}
-	}
-	int *har;
-	har = new int [n];
-	for (i = 0; i < n; i++)
-	{
-		har[i] = 0;
-		for (j = 0; j < n; j++)
-		{
-			if ((matr[i][j] % 2 == 0) && (matr[i][j] > 0))
-			{
-				har[i] += matr[i][j];
-			}
-		}
+double anotherfunction()
+{
+    int a = 0;
+    int b = a;
+    return (double)b;
+}
 
-	}
-	int *rab;
-	int min;
-	for (i = 0; i < n; i++)
-	{
-		min = har[i];
-		for (j = i; j < n; j++)
-			if (min>har[i])
-			{
-				min = har[i];
-				i = j;
-			}
-		har[j] = har[i];
-		har[i] = min;
-		rab = matr[i];
-			matr[i] = matr[j];
-		matr[j] = rab;
-	}
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			cout << "matrica:" << matr[i][j];
-		}
-	}
+SomeClass::Constructor() : aaaaaaaa(aaaaaaaa), aaaaaaaa(aaaaaaaa), aaaaaaaa(aaaaaaaaaaaaaaaaaaaaaaaaa) { return 0; }
 
-	switch(var) {
-case 0: case 1: case 2: {dosomething();break;}case 4: {
-do_something_else();break;}default: {
-    /*...*/}}
-	
-x = *p;
-p = &x;
-x = r.y;
-x = r->y
-y = x + z--;
-y = x == 0 ? 1 : 0;
+int main()
+{
+    int n;
+    int i = 0;
+    int j = 0;
+    cout << "\n Vvedite rezmernost matrici";
+    cin >> n;
+    int **matrEca;
+    matr = new int *[n];
+    if(matr[i] = NULL) {
+        cout << "Nesozdandinamicheskiymassiv";
+        return 0;
+    }
+    for(int i = 0; i < n; i++) {
+        matr[i] = new int[n];
+        if(matr[i] = NULL) {
+            cout << "Ne sozdan dinamicheskiy massiv";
+            return 0;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << "vvedite massiv";
+            cin >> matr[i][j];
+        }
+    }
+    int *har;
+    har = new int[n];
+    for(i = 0; i < n; i++) {
+        har[i] = 0;
+        for(j = 0; j < n; j++) {
+            if((matr[i][j] % 2 == 0) && (matr[i][j] > 0)) {
+                har[i] += matr[i][j];
+            }
+        }
+    }
+    int *rab;
+    int min;
+    for(i = 0; i < n; i++) {
+        min = har[i];
+        for(j = i; j < n; j++)
+            if(min > har[i]) {
+                min = har[i];
+                i = j;
+            }
+        har[j] = har[i];
+        har[i] = min;
+        rab = matr[i];
+        matr[i] = matr[j];
+        matr[j] = rab;
+    }
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            cout << "matrica:" << matr[i][j];
+        }
+    }
 
-	return 0;
+    switch(var) {
+    case 0:
+    case 1:
+    case 2: {
+        dosomething();
+        break;
+    }
+    case 4: {
+        do_something_else();
+        break;
+    }
+    default: {
+    /*...*/}
+    }
+
+    x = *p;
+    p = &x;
+    x = r.y;
+    x = r->y y = x + z--;
+    y = x == 0 ? 1 : 0;
+
+    return 0;
 }
